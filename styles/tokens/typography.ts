f052740da909d1681d2a5248e30f1865bbf64ec4
PerePonsPons <perepons8@gmail.com>
2026-07-21 22:09:44 +0200

    feat: add global styles

---
+++ styles/tokens/typography.ts
@@ -0,0 +1,18 @@
+export const typography = {
+  fontFamily: {
+    sans: "var(--font-geist-sans)",
+  },
+
+  fontWeight: {
+    regular: 400,
+    medium: 500,
+    semibold: 600,
+    bold: 700,
+  },
+
+  lineHeight: {
+    tight: 1.2,
+    normal: 1.5,
+    relaxed: 1.7,
+  },
+} as const;
