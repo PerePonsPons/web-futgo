97e08dede67c933ce8df95c2c4ac802edf8991f8
PerePonsPons <perepons8@gmail.com>
2026-07-21 22:54:57 +0200

    feat: add header layout component

---
+++ components/layout/header.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header>
+      <div>
+        <div>Web Futgo</div>
+      </div>
+    </header>
+  );
+}
--- components/layout/main-layout.tsx
+++ components/layout/main-layout.tsx
@@ -1,9 +1,16 @@
 import type { ReactNode } from "react";
+import { Header } from "./header";
 
 interface MainLayoutProps {
   children: ReactNode;
 }
 
 export function MainLayout({ children }: MainLayoutProps) {
-  return <main>{children}</main>;
+  return (
+    <>
+      <Header />
+
+      <main>{children}</main>
+    </>
+  );
 }
