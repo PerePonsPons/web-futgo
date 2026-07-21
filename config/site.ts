cdc07319639e7a33130014a679cd17b6cc51be32
PerePonsPons <perepons8@gmail.com>
2026-07-22 00:03:52 +0200

    pas 22 provisional

--- config/site.ts
+++ config/site.ts
@@ -1,3 +1,10 @@
 export const siteConfig = {
   name: "Web Futgo",
-};
+
+  description:
+    "Web corporativa especialitzada en campus, tecnificacions, clínics i experiències de futbol base a Menorca.",
+
+  url: "https://webfutgo.local",
+
+  locale: "es-ES",
+} as const;
