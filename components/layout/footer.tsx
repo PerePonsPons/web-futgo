e826821ab472618fc5c651e3f7cb5b2c2bd75d2d
PerePonsPons <perepons8@gmail.com>
2026-07-23 20:14:08 +0200

    chore: final quality review

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -2,7 +2,7 @@ import { Container } from "@/components/ui/container";
 
 export function Footer() {
   return (
-    <footer>
+    <footer aria-label="Peu de pàgina">
       <Container>
         <div>© Web Futgo</div>
       </Container>
